fd73b865961fabcd6ced0ea71fdf4bf337f7a640
Fanasina Ramalandimanana <hope.fanasina2@gmail.com>
2023-09-12 05:32:39 +0300

    SECOND VERSION OF ROCK-PAPER-SCISSORS GAME

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const btns = document.querySelectorAll('.btn');
 const computerDiv = document.getElementById('computer');
+const pResult = document.getElementById('resultP');
 let isClicked = false;
 btns.forEach(function (btn, index) {
   btn.addEventListener('click', function () {
-    if (!isClicked) {
-      btn.style.animationName = 'scale';
-      btn.classList.add('active');
-      computerDiv.style.animationName = 'bg';
-      isClicked = true;
-    }
+    btn.style.animationName = 'scale';
+    btn.classList.add('active');
+    computerDiv.style.animationName = 'bg';
+    isClicked = true;
     let yourMove;
+
+    btn.style.transition = '1s';
+    btn.style.zIndex = "100";
+    btn.style.transform = 'scale(2)';
+
     if (index == 0) {
       yourMove = 'Rock'
     } else if (index == 1) {
@@ -19,24 +23,34 @@ btns.forEach(function (btn, index) {
     } else {
       yourMove = 'Unknown'
     }
+
     setTimeout(function () {
       btn.style.animationName = '';
-      computerDiv.style.animationName = ' ';
-    }, 1000);
-    console.log(yourMove);
-    let computerRdm = Math.random();
+      btn.classList.remove('active');
+      computerDiv.style.animationName = '';
+      btn.style.transition = '';
+      btn.style.position = '';
+      btn.style.zIndex = "3";
+      btn.style.transform = 'scale(1)';
+    }, 3000);
+    // console.log(yourMove);
+    const computerRdm = Math.random();
     let computerMove;
     if (computerRdm < 1 / 3) {
       computerMove = 'Rock';
-      computerDiv.classList.add('Rock');
+      computerDiv.classList.add(computerMove);
+      computerDiv.classList.remove('Paper', 'Scissors');
+
     } else if (computerRdm > 1 / 3 && computerRdm < 2 / 3) {
       computerMove = 'Paper';
-      computerDiv.classList.add('Paper');
+      computerDiv.classList.add(computerMove);
+      computerDiv.classList.remove('Rock', 'Scissors');
     } else if (computerRdm > 2 / 3) {
       computerMove = 'Scissors';
-      computerDiv.classList.add('Scissors');
+      computerDiv.classList.add(computerMove);
+      computerDiv.classList.remove('Rock', 'Paper');
     }
-    console.log(`Voici le move du PC : ${computerMove}`);
+    // console.log(`Voici le move du PC : ${computerMove}`);
     let result;
     if (computerMove == 'Rock' && yourMove == 'Scissors') {
       result = "LOSE";
@@ -55,6 +69,22 @@ btns.forEach(function (btn, index) {
     else if (computerMove == yourMove) {
       result = 'Tie';
     }
-    console.log(`Voici le resultat : ${result}`)
-  })
+    // console.log(`Voici le resultat : ${result}`)
+    setTimeout(function () {
+      pResult.innerHTML = result;
+      pResult.classList.add('show');
+      if (result == 'WIN') {
+        pResult.style.backgroundColor = 'lightgreen';
+      } else if (result == 'LOSE') {
+        pResult.style.backgroundColor = 'tomato';
+      } else if (result == 'Tie') {
+        pResult.style.backgroundColor = 'lightgray';
+      }
+    }, 2500)
+    setTimeout(function () {
+      pResult.classList.remove('show');
+    }, 4000)
+
+  }
+  )
 })
