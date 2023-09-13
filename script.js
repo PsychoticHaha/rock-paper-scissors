5bab2e792feb51e56c8c7d69003f8fcddec0e129
Fanasina Ramalandimanana <hope.fanasina2@gmail.com>
2023-09-14 00:50:54 +0300

    SIMPLE ROCK PAPER SCISSORS GAME

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const btns = document.querySelectorAll('.btn');
 const computerDiv = document.getElementById('computer');
 const pResult = document.getElementById('resultP');
+const scoreBoard = document.getElementById('scoreBoard');
 let isClicked = false;
+let score;
 btns.forEach(function (btn, index) {
-  btn.addEventListener('click', function () {
+  score = {
+    WIN: 0,
+    LOSE: 0,
+    TIE: 0
+  };
+  btn.addEventListener('click', function clickedBtn() {
+    computerDiv.style.animationName = 'bg';
     btn.style.animationName = 'scale';
     btn.classList.add('active');
-    computerDiv.style.animationName = 'bg';
     isClicked = true;
     let yourMove;
-
-    btn.style.transition = '1s';
-    btn.style.zIndex = "100";
-    btn.style.transform = 'scale(2)';
+    btn.style = 'transition:1s;z-index:100;transform:scale(2);position:absolute';
 
     if (index == 0) {
       yourMove = 'Rock'
@@ -27,13 +31,10 @@ btns.forEach(function (btn, index) {
     setTimeout(function () {
       btn.style.animationName = '';
       btn.classList.remove('active');
-      computerDiv.style.animationName = '';
-      btn.style.transition = '';
-      btn.style.position = '';
-      btn.style.zIndex = "3";
-      btn.style.transform = 'scale(1)';
-    }, 3000);
-    // console.log(yourMove);
+      computerDiv.style = "animation-name:'';";
+      computerDiv.attributes.removeNamedItem('class');
+      btn.style = 'transition:"0.3s"; position:"";z-index:3;transform:scale(1)';
+    }, 5000);
     const computerRdm = Math.random();
     let computerMove;
     if (computerRdm < 1 / 3) {
@@ -50,41 +51,61 @@ btns.forEach(function (btn, index) {
       computerDiv.classList.add(computerMove);
       computerDiv.classList.remove('Rock', 'Paper');
     }
-    // console.log(`Voici le move du PC : ${computerMove}`);
     let result;
     if (computerMove == 'Rock' && yourMove == 'Scissors') {
       result = "LOSE";
+      score.LOSE++;
     } else if (computerMove == 'Scissors' && yourMove == 'Paper') {
       result = 'LOSE';
+      score.LOSE++;
     } else if (computerMove == 'Paper' && yourMove == 'Rock') {
       result = 'LOSE';
+      score.LOSE++;
+
     } else if (computerMove == 'Paper' && yourMove == 'Scissors') {
       result = "WIN";
+      score.WIN++;
     } else if (computerMove == 'Scissors' && yourMove == 'Rock') {
+      score.WIN++;
       result = 'WIN';
     } else if (computerMove == 'Rock' && yourMove == 'Paper') {
       result = 'WIN';
-
+      score.WIN++;
     }
     else if (computerMove == yourMove) {
       result = 'Tie';
+      score.TIE++;
     }
-    // console.log(`Voici le resultat : ${result}`)
+    // console.log(`LOSE : ${score.LOSE} \nWIN : ${score.WIN} \nTIE : ${score.TIE}`);
+
     setTimeout(function () {
       pResult.innerHTML = result;
       pResult.classList.add('show');
       if (result == 'WIN') {
-        pResult.style.backgroundColor = 'lightgreen';
+        pResult.style = 'background-color:lightgreen;';
       } else if (result == 'LOSE') {
-        pResult.style.backgroundColor = 'tomato';
+        pResult.style = 'background-color:tomato;color:#e8e8e8; ';
       } else if (result == 'Tie') {
-        pResult.style.backgroundColor = 'lightgray';
+        pResult.style = 'background-color:lightgray; color: black;';
       }
-    }, 2500)
+    }, 2000)
     setTimeout(function () {
       pResult.classList.remove('show');
     }, 4000)
-
   }
   )
 })
+function reset() {
+  score.WIN = 0;
+  score.LOSE = 0;
+  score.TIE = 0;
+  scoreBoard.innerHTML = 'Score Reset';
+  scoreBoard.style = 'transform:translate(0)';
+};
+function showScore() {
+  scoreBoard.innerHTML = `Voici le score <br> WIN : ${score.WIN} <br>LOSE : ${score.LOSE} <br>TIE : ${score.TIE}`;
+  scoreBoard.style = 'transform:translate(0)';
+  setTimeout(() => {
+    scoreBoard.style = 'transform:';
+  }, 5000)
+}
